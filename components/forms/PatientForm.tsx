6049d0b4fce1702d945d15277a5e65f8f5ac241c
Vaibhav Bhalla <bhallavbv@gmail.com>
2024-07-13 08:25:19 +0530

    Add custom form field - email to patient form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -53,6 +53,15 @@ const PatientForm = () => {
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@example.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
