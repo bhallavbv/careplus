98636489953c2f037f91fc5c35813dc6523d5283
Vaibhav Bhalla <bhallavbv@gmail.com>
2024-07-13 12:20:26 +0530

    Add form field Type - phone input

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,6 +13,9 @@ import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
 import Image from "next/image";
+import { E164Number } from "libphonenumber-js/core";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 interface CustomProps {
   control: Control<any>;
@@ -55,6 +58,20 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             />
           </FormControl>
         </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="US"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
       );
     default:
       break;
