70219376fff2c83d3d36f0ef63c06b891c1232e9
Vaibhav Bhalla <bhallavbv@gmail.com>
2024-07-13 08:02:38 +0530

    Create custom component for form field
    create fieldType enum
    create properties that can be passed to a field

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -11,24 +11,43 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
 
 interface CustomProps {
   control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({ control }: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  return <Input type="text" placeholder="John Doe" />;
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label, placeholder } = props;
   return (
     <FormField
       control={control}
       name="username"
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input placeholder="shadcn" {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
+          {/* {form.formState.errors[name]?.message} */}
         </FormItem>
       )}
     />
