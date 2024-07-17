7124237f2ea46fd567039cc56c013a616f4ee88a
Vaibhav Bhalla <bhallavbv@gmail.com>
2024-07-17 08:56:20 +0530

    Add patient action to getUser

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,5 +1,6 @@
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -19,5 +20,15 @@ export const createUser = async (user: CreateUserParams) => {
       return documents.users[0];
     }
     console.log("Error while creating user::", error);
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    console.log("User fetched successfully::", user);
+    return parseStringify(user);
+  } catch (error) {
+    console.log("Error while fetching user::", error);
   }
 };
